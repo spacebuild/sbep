83373addb56acf88fa31e07ffa6b6569bfe46dfb
Selekton99 <samcooke98@yahoo.com.au>
2012-11-28 14:45:37 +1000

    Some of this will need testing. The welcome menu will be bugged but the vehicles should be re-added and not spawn in the ground
    Also as a TODO: Beta.txt determines that the version is a beta for the welcome menu

--- lua/autorun/client/cl_sbep_menu.lua
+++ lua/autorun/client/cl_sbep_menu.lua
@@ -6,10 +6,26 @@
 -- Last Updated :  
 --
 
+--Australian Localization: (Because my spelling is bad enough, I don't need to be americanised)
+panel.SetColour = panel.SetColor
+Colour = Color
+
+
+serverVersion = "Unknown"
+print("This server has version: "..serverVersion.."\n")
+clientVersion = "Unknown"
+
+function ServerHaveSBEP()
+	-- Because I know of at least one SB server without SBEP, Amazing right?
+	if (ServerVersion = "Unknown") then
+		return false
+	else
+		return true
+end
 
 
 function CreateMenu( )
-	if (ServerHaveSBEP() == false ) then return false end
+	if (ServerHaveSBEP() == false ) then return end
 	local ClVersion = GetClientSVN()
 	local SvVersion = RequestServerSVN()
 	local Latest = GetLatestSVN()
@@ -30,18 +46,37 @@ function CreateMenu( )
 	PropSheet:SetSize( 295, 295 )
 
 
-	--Stuff to check Server Version
+	--Category to check Server SVN info
 	local ServerCat = vgui.Create("DCollapsibleCategory")
 	ServerCat:SetParent(PropSheet)
 	ServerCat:SetSize( 200, 25 )
-	ServerCat:SetPos( )     --TODO: Figure out pos for Collapsible Category
+	ServerCat:SetPos( )     --TODO: Figure out positions for Collapsible Category
 	ServerCat:SetLabel("Server")
 
-	local ServerCatCg = vgui.Create("DPanelList")
-	ServerCatCg:SetAutoSize(true)
-	ServerCatCg:SetSpacing( 5 )
-	ServerCatCg:EnableHorizontal( false )
-	ServerCatCg:EnableVerticalScrollbar( true )
+	local BackgroundSV = vgui.Create("DPanel")    -- Create Main info stuff
+	BackgroundSV:SetPosition()
+	BackgroundSV:SetSize()
+	BackgroundSV:SetParent(ServerCat)
+
+	--[[ Disabled till the system works as this is just for looks!
+	local ColourWarning = vgui.Create("DPanel")
+	ColourWarning:SetPosition(0,0) --TODO: Figure out positions
+	ColourWarning:SetSize() --TODO:Figure out size
+	ColourWarning:SetParent(ServerCat)
+	ColourWarning:SetColour(Color(255,0,0,255))
+	]]--
+
+	local SVInfo = vgui.Create("DLabel")
+	SVInfo:Text("The Server has Version: "..serverVersion.."") -- TODO: Check if this can be autowrapped around. The next line must say The Latest Version is and then The Server is ... behind (or) The Server is up to date.
+	SVInfo:SetWrap(true)
+	SVInfo:SetParent(ServerCat)
+	SVInfo:SetText("The Server has version: "..serverVersion..". \n The latest Version is "..latest..". The Server is "..toint(latest) - toint(serverVersion).. " versions behind."
+
+
+	-- Then we will change the colour of ColourWarning to notify it is behind (This is purely for looks) In fact it
+	-- won't work yet! Because I want to get this menu done with and pretty it up later. (I hate derma)
+
+
 
 
 
@@ -58,8 +93,9 @@ function CreateMenu( )
 
 
 end
+concommand.Add("sbep_menu", CreateMenu, nil, "Open the SBEP welcome menu" )
 
 
 if CLIENT then
-hook.Add("Initialize","Create SBEP Menu", CreateMenu )
+	hook.Add("Initialize","Create SBEP Menu", CreateMenu )
 end
