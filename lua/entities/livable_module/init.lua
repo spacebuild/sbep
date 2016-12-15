f56f182b0f0a793b639b2b9ba6550dd8c9f1ccf4
X-Coder <gmod@x-coder.de>
2016-12-15 03:38:47 +0100

    Added wire damaged output to livable_module

--- lua/entities/livable_module/init.lua
+++ lua/entities/livable_module/init.lua
@@ -18,7 +18,7 @@ function ENT:Initialize()
 	if not (WireAddon == nil) then
 		self.WireDebugName = self.PrintName
 		self.Inputs = Wire_CreateInputs(self, { "On", "Gravity", "Max O2 level" })
-		self.Outputs = Wire_CreateOutputs(self, { "On", "Oxygen-Level", "Temperature", "Gravity" })
+		self.Outputs = Wire_CreateOutputs(self, { "On", "Oxygen-Level", "Temperature", "Gravity", "Damaged" })
 	else
         self.Inputs = { { Name = "On" }, { Name = "Radius" }, { Name = "Gravity" }, { Name = "Max O2 level" } }
     end
@@ -513,6 +513,7 @@ function ENT:Climate_Control()
 		Wire_TriggerOutput(self, "Oxygen-Level", tonumber(self:GetO2Percentage()))
 		Wire_TriggerOutput(self, "Temperature", tonumber(self.sbenvironment.temperature))
 		Wire_TriggerOutput(self, "Gravity", tonumber(self.sbenvironment.gravity))
+		Wire_TriggerOutput(self, "Damaged", tonumber(self.damaged))
 	end
 end
 
